87e83f528074b59cc8d2953bec4a2f8241dc33fa
SNEHALDD <106944351+SNEHALDD@users.noreply.github.com>
2022-06-22 11:03:25 -0500

    Add Variables
    Add different types of variables

---
+++ pytest.py
@@ -0,0 +1,26 @@
+# Creates a variable with a string "Frankfurter"
+title = "Frankfurter"
+
+
+# Creates a variable with an integer 80
+a = 80
+
+#print a title with a integer
+print(title +  " " + str(a))
+
+# Creates a variable with the boolean value of True
+expert_status = True
+
+# Prints a statement adding the variable
+print("Nick is a professional " + title)
+
+
+# Convert the integer years into a string and prints
+print("he has been coding for " + str(a) + " years")
+
+
+# Converts a boolean into a string and prints
+print("Expert_status: " + str(expert_status))
+
+# An f-string accepts all data types without conversion
+print(f"expert_status: {expert_status}")
