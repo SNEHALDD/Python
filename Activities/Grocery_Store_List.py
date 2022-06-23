3f40e8fdf7c56157f598c1d98e8ed879bbbf990c
SNEHALDD <106944351+SNEHALDD@users.noreply.github.com>
2022-06-23 13:40:13 -0500

    Grocery list
    Tried to add, remove, replace grocery items

---
+++ Activities/Grocery_Store_List.py
@@ -0,0 +1,17 @@
+# Create a Python list to store your grocery list
+grocery_List = ["Banana", "Apple","peanut Butter", "Cilantro", "Chicken"]
+
+# Print the grocery list
+print(grocery_List)
+
+# Change "Peanut Butter" to "Almond Butter" and print out the updated list
+grocery_List [2] = "Almond Butter"
+print(grocery_List)
+
+# Remove "Cilantro" from grocery list and print out the updated list
+grocery_List.remove("Cilantro")
+print(grocery_List)
+
+# Add "Coffee" to grocery list and print the updated list
+grocery_List.append("Coffee")
+print(grocery_List)
